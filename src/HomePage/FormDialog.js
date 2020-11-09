da03df2824aeb2f2fc4e3d51760ac42615bf9388
MenkeTechnologies <linux.dev25@gmail.com>
2020-11-09 18:53:41 -0500

    VNT-32 dont use button inside of menu

--- src/HomePage/FormDialog.js
+++ src/HomePage/FormDialog.js
@@ -1,39 +1,34 @@
 import React from 'react';
 import FormData from '../Registration/FormData';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
+import ListItemText from "@material-ui/core/ListItemText";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
+    return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export default function FormDialog() {
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  return (
-    <div>
-      <Button onClick={handleClickOpen}>
-        Sign-Up
-      </Button>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" TransitionComponent={Transition}>
-        <DialogContent>
-          <FormData/>
-        </DialogContent>
-        {/* <DialogActions>
+    return (
+        <div>
+            <ListItemText onClick={handleClickOpen} primary="Sign Up"/>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title"
+                    TransitionComponent={Transition}>
+                <DialogContent>
+                    <FormData/>
+                </DialogContent>
+                {/* <DialogActions>
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
@@ -41,7 +36,7 @@ export default function FormDialog() {
             Subscribe
           </Button>
         </DialogActions> */}
-      </Dialog>
-    </div>
-  );
+            </Dialog>
+        </div>
+    );
 }
