565368726d2d5c0b4135f3d77e0648c9446b3101
John Welsh <johnwelsh629@gmail.com>
2020-11-16 15:34:08 -0500

    VNT-12 #comment Dashboard Started

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import Navbar from './Navbar';
 
 class Dashboard extends Component {
   render() {
     return (
-      <div className="HomePage">
-          <h2>Connected</h2>
+      <div className="Dashboard">
+        <div className="Dashboard-Navbar">
+          <Navbar />
+        </div>
+        <div className="Dashboard-Selector">
+
+        </div>
+        <div className="Dashboard-Volunteering">
+
+        </div>
+        <div className="Dashboard-Requests">
+
+        </div>
       </div>
     )
   }
