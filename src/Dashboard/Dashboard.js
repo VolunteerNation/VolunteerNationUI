0e83e0dcd11763fe280e261c79b5d62102d18d89
John Welsh <johnwelsh629@gmail.com>
2020-11-30 15:01:01 -0500

    VNT-49 #comment Added Grid to Cards

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Grid from '@material-ui/core/Grid';
 import Navbar from './Navbar';
 import Card from '../Card/Card.js';
 
@@ -10,21 +11,79 @@ class Dashboard extends Component {
           <Navbar />
         </div>
         <div className="Dashboard-Selector">
-          <Card
-            name = "John Doe"
-            description = "lorem ipsum"
-            category = "Tutoring"
-            city = "Gainesville"
-            state = "FL"
-            status = "New"
-            date = "December 24, 2020"
-          />
         </div>
         <div className="Dashboard-Volunteering">
 
         </div>
         <div className="Dashboard-Requests">
-
+          <Grid container direction="row" spacing={3} justify="space-even">
+            <Grid item sm={6} md={4} lg={3} xl={2}>
+              <Card
+                  name = "John Doe"
+                  description = "lorem ipsum"
+                  category = "Tutoring"
+                  city = "Gainesville"
+                  state = "FL"
+                  status = "New"
+                  date = "December 24, 2020"
+                />
+            </Grid>
+            <Grid item sm={6} md={4} lg={3} xl={2}>
+              <Card
+                  name = "John Doe"
+                  description = "lorem ipsum"
+                  category = "Tutoring"
+                  city = "Gainesville"
+                  state = "FL"
+                  status = "New"
+                  date = "December 24, 2020"
+                />
+            </Grid>
+            <Grid item sm={6} md={4} lg={3} xl={2}>
+              <Card
+                  name = "John Doe"
+                  description = "lorem ipsum"
+                  category = "Tutoring"
+                  city = "Gainesville"
+                  state = "FL"
+                  status = "New"
+                  date = "December 24, 2020"
+                />
+            </Grid>
+            <Grid item sm={6} md={4} lg={3} xl={2}>
+              <Card
+                  name = "John Doe"
+                  description = "lorem ipsum"
+                  category = "Tutoring"
+                  city = "Gainesville"
+                  state = "FL"
+                  status = "New"
+                  date = "December 24, 2020"
+                />
+            </Grid>
+            <Grid item sm={6} md={4} lg={3} xl={2}>
+              <Card
+                  name = "John Doe"
+                  description = "lorem ipsum"
+                  category = "Tutoring"
+                  city = "Gainesville"
+                  state = "FL"
+                  status = "New"
+                  date = "December 24, 2020"
+                />
+            </Grid>
+            <Grid item sm={6} md={4} lg={3} xl={2}>
+              <Card
+                  name = "John Doe"
+                  description = "lorem ipsum"
+                  category = "Tutoring"
+                  city = "Gainesville"
+                  state = "FL"
+                  status = "New"
+                  date = "December 24, 2020"
+                />
+            </Grid>
+          </Grid>
         </div>
       </div>
     )
