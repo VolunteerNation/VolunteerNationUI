f9767d26066a220f75b90ff26b6f242cf865a0a4
monica <57308517+mlewis55@users.noreply.github.com>
2020-12-02 16:49:55 -0500

    .

--- src/Dashboard/NewPostForm.js
+++ src/Dashboard/NewPostForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import RTETextField from './RTETextArea';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography'
 
@@ -24,7 +23,6 @@ export default function NewPostForm() {
                 />
             </Grid>
             <Grid item xs={12}>
-                <RTETextArea/>
             </Grid>
 
             <Grid item xs={12}>
